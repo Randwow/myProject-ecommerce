80f035a82b08740d284b61880c313cec01738d3a
Randwow <makeev.n.00@mail.ru>
2019-01-22 19:53:40 +0200

    SU fix lint issues

---
+++ src/error.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+$(document).ready(function () {
+  showPolygons();
+  setInterval(function () {
+    showPolygons();
+  }, 2500)
+})
+
+function showPolygons() {
+
+  $('[class="processed"]').removeAttr('class');
+  var polyCount = $('polygon').length;
+
+  $('polygon').each(function (ind, el) {
+    setTimeout(function () {
+      $('polygon:eq(' + ind + ')').attr('class', 'processed');
+    }, Math.random() * 2000);
+  });
+}
